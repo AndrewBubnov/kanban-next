e74c8cb163d4ce59f0eb9d2496414c908ca6263b
Max Bubnov <m.bubnov05@gmail.com>
2023-10-15 20:54:57 +0200

    Refactor

--- src/components/AddTask/AddTask.tsx
+++ src/components/AddTask/AddTask.tsx
@@ -4,20 +4,21 @@ import TextField from '@mui/material/TextField';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import { Box, Grid } from '@mui/material';
-import { StyledModalButton, StyledDialog, StyledDialogActions, StyledAddButton } from '@/components/StyledComponents';
+import { StyledModalButton, StyledDialog, StyledAddButton } from '@/components/StyledComponents';
 import { addTask } from '@/prismaActions/addTask';
 
 export const AddTask = ({ userId }: { userId: string }) => {
 	const [openModal, setOpenModal] = useState<boolean>(false);
 	const toggleOpenHandler = () => setOpenModal(prevState => !prevState);
 	const confirmHandler = async () => {
-		await addTask({ title, body }, userId);
+		if (!title) return;
+		await addTask({ title, body: description }, userId);
 		setOpenModal(false);
 	};
 	const [title, setTitle] = useState<string>('');
-	const [body, setBody] = useState<string>('');
+	const [description, setDescription] = useState<string>('');
 	const titleHandler = (evt: ChangeEvent<HTMLInputElement>) => setTitle(evt.target.value);
-	const bodyHandler = (evt: ChangeEvent<HTMLInputElement>) => setBody(evt.target.value);
+	const descriptionHandler = (evt: ChangeEvent<HTMLInputElement>) => setDescription(evt.target.value);
 	return (
 		<>
 			<Grid container mt={1}>
@@ -28,19 +29,37 @@ export const AddTask = ({ userId }: { userId: string }) => {
 			<StyledDialog open={openModal} onClose={toggleOpenHandler}>
 				<DialogTitle>Create new task</DialogTitle>
 				<DialogContent>
-					<Box>Task title</Box>
-					<TextField onChange={titleHandler} margin="dense" variant="standard" fullWidth />
-					<Box mt={2}>Task body</Box>
-					<TextField onChange={bodyHandler} margin="dense" variant="outlined" fullWidth multiline rows={4} />
+					<form action={confirmHandler}>
+						<Box>Task title</Box>
+						<TextField
+							onChange={titleHandler}
+							value={title}
+							name="title"
+							margin="dense"
+							variant="standard"
+							fullWidth
+						/>
+						<Box mt={2}>Task body</Box>
+						<TextField
+							onChange={descriptionHandler}
+							value={description}
+							margin="dense"
+							variant="outlined"
+							name="body"
+							fullWidth
+							multiline
+							rows={4}
+						/>
+						<Grid container flexDirection="row-reverse">
+							<StyledModalButton variant="text" onClick={toggleOpenHandler}>
+								Cancel
+							</StyledModalButton>
+							<StyledModalButton type="submit" variant="text">
+								Confirm
+							</StyledModalButton>
+						</Grid>
+					</form>
 				</DialogContent>
-				<StyledDialogActions>
-					<StyledModalButton variant="text" onClick={toggleOpenHandler}>
-						Cancel
-					</StyledModalButton>
-					<StyledModalButton variant="text" onClick={confirmHandler}>
-						Confirm
-					</StyledModalButton>
-				</StyledDialogActions>
 			</StyledDialog>
 		</>
 	);
--- src/components/StyledComponents.ts
+++ src/components/StyledComponents.ts
@@ -1,7 +1,6 @@
 'use client';
 import { styled } from '@mui/material/styles';
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
 import { Toolbar } from '@mui/material';
 
@@ -11,15 +10,14 @@ export const StyledDialog = styled(Dialog)(() => ({
 	},
 }));
 
-export const StyledDialogActions = styled(DialogActions)(() => ({
-	padding: '8px 20px 8px 8px',
-}));
-
 export const StyledModalButton = styled(Button)(() => ({
 	'textTransform': 'none',
 	'color': 'rgba(0, 0, 0, 0.87)',
 	'&:focus': {
 		outline: 'none',
+	},
+	'&:hover': {
+		color: '#000',
 	},
 }));
 
