494363ef67f5deda9277dc63cf8112cf5bd2ddf1
Max Bubnov <m.bubnov05@gmail.com>
2023-11-02 16:10:59 +0100

    refactor updateIndices

--- src/utils/updateIndices.ts
+++ src/utils/updateIndices.ts
@@ -1,6 +1,23 @@
 import { Parameters, TaskItem } from '@/types';
 
-export const updateIndices = (config: TaskItem[], pos: Parameters) =>
-	config
-		.sort((el1, el2) => pos[el1.id].top + pos[el1.id].dY - pos[el2.id].top - pos[el2.id].dY)
-		.map((el, index) => ({ ...el, index }));
+export const updateIndices = (config: TaskItem[], pos: Parameters) => {
+	const Map = config.reduce(
+		(acc, cur) => {
+			if (!acc[cur.status]) {
+				acc[cur.status] = [cur];
+				return acc;
+			} else {
+				acc[cur.status].push(cur);
+				return acc;
+			}
+		},
+		{} as Record<string, TaskItem[]>
+	);
+	return Object.values(Map)
+		.map(el =>
+			el
+				.sort((el1, el2) => pos[el1.id].top + pos[el1.id].dY - pos[el2.id].top - pos[el2.id].dY)
+				.map((el, index) => ({ ...el, index }))
+		)
+		.flat();
+};
