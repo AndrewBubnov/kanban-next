fc7d718b57133eecd6ec48b8131e4a9f0a927245
Max Bubnov <m.bubnov05@gmail.com>
2023-11-24 14:11:05 +0100

    Fix error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,15 +2,17 @@
 
 import { useEffect } from 'react';
 import { HeroContainer, StyledActionButton } from '@/modules/Shared/styled';
+import { useTheme } from 'next-themes';
 
 export default function Error({ reset, error }: { reset(): void; error: Error & { digest?: string } }) {
+	const { theme } = useTheme();
 	useEffect(() => console.error(error), [error]);
 	return (
 		<div>
 			<HeroContainer>
-				Sorry, something went wrong..
-				<StyledActionButton onClick={reset} isLight>
-					Try again
+				Sorry, something went wrong
+				<StyledActionButton onClick={reset} isLight={theme !== 'dark'}>
+					Please try again
 				</StyledActionButton>
 			</HeroContainer>
 		</div>
